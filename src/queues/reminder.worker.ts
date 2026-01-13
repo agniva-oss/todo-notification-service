ca4b4253fd772a5f2e0ea0b89a0f63c60d0d1b55
agniva-oss <agniva@kofukuhcs.com>
2026-01-14 00:12:58 +0530

    updated worker

--- src/queues/reminder.worker.ts
+++ src/queues/reminder.worker.ts
@@ -9,7 +9,8 @@ new Worker(
       const { todo_id, message, due_date } = job.data;
       console.log(job , 'job data in worker');
       const type = job.name === "reminder" ? "REMINDER" : "TODO_EVENT";
-      await NotificationService.createManual(todo_id, message, type, "Todo Notification", due_date);
+      const title = job.name === "reminder" ? "Reminder Notification" : "Todo Notification";
+      await NotificationService.createManual(todo_id, message, type, title, due_date);
       console.log(`Notification sent for todo: ${todo_id}`);
     } catch (error) {
       console.error('Error processing notification job:', error);
